743f6fe3e077dc493793f73b386a47102431e7ba
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2020-07-08 20:16:05 +0200

    changed API to https

--- src/data/fetch/productsFetch.js
+++ src/data/fetch/productsFetch.js
@@ -1,6 +1,6 @@
 export const fetchProducts = () => {
   const promise = fetch(
-    "http://jsdeveloper-shop-api.s3-website-eu-west-1.amazonaws.com/"
+    'https://d24huwa7xw9s1p.cloudfront.net/?fbclid=IwAR3be8g2pG_aaRu8Cg1QyhVJACiZJiTytst48DdKwX_0tuQGGbTnbPrOFqE',
   );
 
   return promise;
